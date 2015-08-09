edd0aa8b135075818936c046574488ba3ddf2872
stringcode <michael@stringcode.co.uk>
2015-08-09 21:53:55 +0100

    first commit

---
+++ Pure Awesome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pure Awesome
+//
+//  Created by Michal Inger on 08/08/2015.
+//  Copyright © 2015 stringCode ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
